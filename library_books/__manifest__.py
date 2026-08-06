beff7f8341191d90dc174ca2c1b57829b5325d63
Abinjoshua <abin@cybrosys.com>
2026-08-06 17:19:23 +0530

    Saas

---
+++ library_books/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Library Books',
+    'version': '1.0',
+    'summary': 'Simple library book tracker built with XML-only customization',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'views/library_book_views.xml',
+        'security/ir.access.csv',
+    ],
+}
---
+++ loyalty_points/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Loyalty Points',
+    'version': '1.0',
+    'summary': 'Adds a loyalty points field to Contacts',
+    'depends': ['base'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+}
